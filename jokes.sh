97f92f310397315e5b555511324c7772c56bd029
oospruel <137451524+oospruel@users.noreply.github.com>
2023-06-24 04:25:46 -0400

    Update jokes.sh

--- jokes.sh
+++ jokes.sh
@@ -1,1 +1,38 @@
+
+
+
+
+
+
+
 #!bin/bash
+quit=1
+again() {
+  echo "(1) try another"
+  echo "(2) quit"
+  read $YorN ;
+case $YorN in
+     1) 
+     2) joke_menu ;;
+esac
+joke_menu() { 
+   echo "select a joke"
+   echo "(1)
+   echo "(2)
+   echo "(3)
+   echo "(4)
+   echo "(5)
+   echo "(6) quit"
+read $jokenum 
+case $jokenum in
+  1)
+  2)
+  3)
+  4)
+  5)
+  6) 
+  *
+esac
+ 
+   
+   
