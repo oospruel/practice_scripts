b078b418af83f99988f6cca9537ad976e1b98c57
oospruel <137451524+oospruel@users.noreply.github.com>
2023-07-07 00:22:45 -0400

    Add files via upload

---
+++ doc_word_counter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+file_name = input("Enter file path ") 
+# try to open file
+try:
+    f = open(file_name)
+except:
+    print("not a valid path for file. Try again.")
+    exit()
+# create empty dicionary
+tmpdic = {}
+# strip lins into words, and add words to empty dictionary
+for fn in f :
+    fn = fn.strip()
+    wds = fn.split()
+    for splitw in wds:
+        tmpdic[splitw] = tmpdic.get(splitw, 0) + 1
+
+#create sorted tuples
+tmptup = sorted( [ (v,k) for k,v in tmpdic.items()], reverse=True)
+# create empty list
+newtuplist = []
+#rearrange tuple and add to the new empty list
+for v,k in tmptup :
+    newtup = k,v
+    newtuplist.append(newtup)
+
+
+print("top 15 repeated words ", newtuplist[:15])
+
+    
+
