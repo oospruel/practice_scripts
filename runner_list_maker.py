5b2a91f24305b42f74c19f497f4be2e4a0b3dfbc
oospruel <137451524+oospruel@users.noreply.github.com>
2023-07-16 10:58:40 -0400

    Add files via upload

--- runner_list_maker.py
+++ runner_list_maker.py
@@ -1,9 +1,11 @@
-with open("runner_list.txt", "r") as file :
-    total_runners = 1
+#This script creates a list of runners
+total_runners = 1
+with open("runner_list.txt", "r+") as file :
+    
     for lines in file:
-        total_runners = total_runners + 1
+        total_runners = total_runners + 1 #adds to keep runner number updated
 
-with open("runner_list.txt", "w") as file :
+with open("runner_list.txt", "a") as file :
     
     name = input("Enter runner {} name: ".format(total_runners))
     file.write("{} is runner {}".format(name, total_runners))
